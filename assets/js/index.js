855d687156213c1cc0c4eaf11b7710bdafe781af
ferideikbaldurmus <ferideikbaldurmus@gmail.com>
2024-06-26 17:07:39 +0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,5 +9,6 @@ import Button from 'react-bootstrap/Button';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Button variant="primary">Primary</Button>
   </React.StrictMode>
 );
